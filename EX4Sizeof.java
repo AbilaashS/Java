ffcce7d05a8bb0b93ecbfc8d83cb916a1e84ebb3
AbilaashS <89683388+AbilaashS@users.noreply.github.com>
2021-08-28 15:17:45 +0530

    Add files via upload

---
+++ EX4Sizeof.java
@@ -0,0 +1,9 @@
+public class EX4Sizeof {
+
+	public static void main(String[] args) {
+		System.out.println("Integer : "+ (Integer.SIZE)/8 + " Bytes");
+		System.out.println("Character : "+ (Character.SIZE)/8 + " Bytes");
+		System.out.println("Float : "+ (Float.SIZE)/8 + " Bytes");
+	}
+
+}
---
+++ Ex3_variable.java
@@ -0,0 +1,12 @@
+public class Ex3_variable {
+
+	public static void main(String[] args) {
+		byte a;
+		int b;
+		char c;
+		double d;
+		float f;
+		boolean g;
+	}
+
+}
