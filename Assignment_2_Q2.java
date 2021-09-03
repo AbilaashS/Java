dad09073e3fe77c560c0a8a5af31671d296a2ea7
AbilaashS <89683388+AbilaashS@users.noreply.github.com>
2021-09-03 14:32:44 +0530

    Add files via upload

---
+++ Assignment_2_Q2.java
@@ -0,0 +1,13 @@
+public class Assignment_2_Q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20,b=3;
+		System.out.println("a="+a+"; b="+b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("After Swap : a="+a+" ; b="+b);
+	}
+
+}
---
+++ Assignment_2_Q3.java
@@ -0,0 +1,10 @@
+public class Assignment_2_Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float si,r=5;
+		int p=1000,n=3;
+		si = p*n*r/100;
+		System.out.println("Interest = "+si);
+	}
+}
