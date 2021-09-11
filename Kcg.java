386641d177d77651e9eaf9605f0a11a9ba19fd29
AbilaashS <89683388+AbilaashS@users.noreply.github.com>
2021-09-11 15:14:51 +0530

    Add files via upload

---
+++ Kcg.java
@@ -0,0 +1,22 @@
+
+
+
+
+
+	public class Kcg
+
+	{
+
+	public static void main(String arg[])
+
+	{
+
+	char a='s';
+
+	int s=95;
+
+	a=(char)s;
+
+	}
+
+	}
---
+++ data_conversion.java
@@ -0,0 +1,13 @@
+package data_conversion;
+
+public class data_conversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		
+		System.out.println(" Int a = "+a+" Converted to long : "+(long)a);
+		System.out.println(" Long a = "+a+" Converted to float : "+(float)a);
+	}
+
+}
